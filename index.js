8687136640c3f4861dcc61d37a96c54a8446e834
Risu Singh <risusingh35@gmail.com>
2024-07-30 13:51:04 +0530

    PrototypalInheritance

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ console.log("---------Index---------");
 // require('./src/ES6/ArrowFunctions')
 // require('./src/ES6/MapSetWeakSetWeakMap')
 // require('./src/ES6/ProxyObject')
-require('./src/ES6/PrototypalInheritance')
+// require('./src/ES6/PrototypalInheritance')
 // require('./src/ES6/OthersEs6Feature')
 
 // require('./src/ETC/ScopeAndClosures')
--- src/ES6/PrototypalInheritance.js
+++ src/ES6/PrototypalInheritance.js
@@ -1,1 +1,13 @@
 console.log("---------PrototypalInheritance---------");
+
+const Animal={
+    eat:true
+}
+
+const dog=Object.create(Animal)
+console.log(dog);
+console.log(dog.barks);
+dog.barks=true
+console.log(dog.barks);
+console.log(dog.eat);
+console.log(dog);
