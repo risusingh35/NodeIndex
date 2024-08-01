39330672453fe051ad80ef7a45fe383fde06881a
Risu Singh <risusingh35@gmail.com>
2024-08-01 10:08:56 +0530

    all js file mode to js folder

---
+++ src/JS/index.js
@@ -0,0 +1,47 @@
+console.log("---------JS Index---------");
+// require('./ES5/StrictMode')
+// require('./ES5/JSONSupport')
+// require('./ES5/ArrayMethods')
+// require('./ES5/GettersAndSetters')
+// require('./ES5/ObjectCreate')
+
+// require('./ES6/BlockScopedVariables_let_const_var')
+// require('./ES6/ArrowFunctions')
+// require('./ES6/MapSetWeakSetWeakMap')
+// require('./ES6/ProxyObject')
+// require('./ES6/PrototypalInheritance')
+// require('./ES6/OthersEs6Feature')
+
+// require('./ETC/ScopeAndClosures')
+// require('./ETC/AsyncAwaitOrderOfExecution')
+// require('./ETC/PromisesChainingAndErrorHandling')
+// require('./ETC/EventLoopTimersAndIO')
+
+// require('./Promise/promise')
+// require('./Promise/BasicPromise')
+// require('./Promise/ChainingPromises')
+// require('./Promise/PromiseAll')
+// require('./Promise/PromiseRace')
+// require('./Promise/PromiseAllSettled')
+// require('./Promise/PromiseAny')
+// require('./Promise/AsynchronousOperations')
+
+// require('./EventLoop/CallStack')
+// require('./EventLoop/Synchronous')
+
+// require('./THIS/1ObjectMethods')
+// require('./THIS/2ConstructorFunctions')
+// require('./THIS/3Classes')
+// require('./THIS/4ClosuresAndThis')
+// require('./THIS/5CallApplyBind')
+
+// require('./Hoisting/VariableHoisting')
+// require('./Hoisting/FunctionAndClassHoisting')
+
+// require('./Closures/createRateLimiterWithClosures')
+// require('./Closures/DataEncapsulation')
+// require('./Closures/loop')
+
+// require('./Memoization/BasicMemoization')
+
+// require('./Currying/CurryingBasic')
