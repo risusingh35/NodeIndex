66d3c558f07885fef30faca403199d051f5fa3bc
risusingh35@gmail.com <risusingh35@gmail.com>
2024-08-12 14:21:55 +0530

    file arrangement

--- src/JS/1DSA/1DsaIndex.js
+++ src/JS/1DSA/1DsaIndex.js
@@ -1,12 +1,12 @@
-import { AlgorithmFibonacciNumbers } from "./1AlgorithmFibonacciNumbers.js";
+import { AlgorithmFibonacciNumbers } from "./DSA_Arrays/1AlgorithmFibonacciNumbers.js";
 import { termsAndDescriptions } from "./helper.js";
-import { binarySearch } from "./2binarySearch.js";
+import { binarySearch } from "./DSA_Arrays/2binarySearch.js";
 import { findSmallest } from "./DSA_Arrays/findSmallest.js";
 import { findLargest } from "./DSA_Arrays/findLargest.js";
 import { BubbleSort } from "./DSA_Arrays/BubbleSort.js";
 import { SelectionSort } from "./DSA_Arrays/SelectionSort.js";
 import { InsertionSort } from "./DSA_Arrays/InsertionSort.js";
-import { Quicksort } from "./Quicksort.js";
+import { Quicksort } from "./DSA_Arrays/Quicksort.js";
 import { LinearSearch } from "./DSA_Arrays/LinearSearch.js";
 import { TimeSort } from "./DSA_Arrays/TimeSort.js";
 const DSAIndex = () => {
