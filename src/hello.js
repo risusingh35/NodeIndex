b95d8c39df00f3ba64cfc0ca14492fe87bc754f4
risusingh35@gmail.com <risusingh35@gmail.com>
2024-08-21 10:34:25 +0530

    nothings

--- src/hello.js
+++ src/hello.js
@@ -4,27 +4,7 @@ export const Hello = () => {
 }
 const CodeRunner = () => {
 
-    // console.log(1);
 
-    // (async () => {
-    //     console.log(2);
-    // })();
-    
-    // console.log(3);
-    
-  
-
-    console.log(1);
-
-    (async function() {
-        console.log(2);
-        var x=await 5
-        console.log(4);
-        
-    })();
-    
-    console.log(3);
-    
 
 
 
